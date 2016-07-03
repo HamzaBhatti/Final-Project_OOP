0c39d15c4677d0b0ac8a2061c5159d074ef5ba45
Hammadshabbir <alihammad295@gmail.com>
2016-07-03 05:46:37 -0700

    Add files via upload

---
+++ project_mainfile.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+#include<fstream>
+#include <fstream>
+using namespace std;
+
+int main(void)
+{
+	
+	
+	cout<<"\n\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2<<ITU LIBRARY MANAGEMENT SYSTEM>>\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 \n"; 
+    cout<<"\n\t\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2****MENU****\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2";
+    cout<<"\n\n\t\t\xDB\xDB\xDB\xDB\xB2 1. IF YOU WANT TO ADD BOOK PRESS 1";
+    cout<<"\n\n\t\t\xDB\xDB\xDB\xDB\xB2 2. IF YOU WANT TO SEARCH ANY BOOK PRESS 2";
+    cout<<"\n\n\t\t\xDB\xDB\xDB\xDB\xB2 3. IF YOU WANT TO ADD A NEW MEMBER PRESS   3";
+    cout<<"\n\n\t\t\xDB\xDB\xDB\xDB\xB2 4. IF YOU WANT TO SEE ALL BOOKS PRESS 4";
+    cout<<"\n\n\t\t\xDB\xDB\xDB\xDB\xB2 6. IF YOU WANT TO ISSUE A BOOK PRESS 5";
+    cout<<"\n\n\t\t\xDB\xDB\xDB\xDB\xB2 7. IF YOU WANT TO RETURN A BOOK PRESS 6 ";
+    cout<<"\n\n\t\t\xDB\xDB\xDB\xDB\xB2 8. IF YOU WANT TO SEE NO OF BOOK(Isued &On Stock) PRESS 7";
+    cout<<" \n\t\t\xDB\xDB\xDB\xDB\xB2 9.  IF YOU WANT TO DISPLAY ALL MEMBER PRESS 8";
+    cout<<"\n\n\t\t\xDB\xDB\xDB\xDB\xB2 10. IF YOU WANT TO EXIT THE PROGRAM PRESS 9";
+    cout<<"\n\n\t\t****************************************************";
+    cout<<"\n\n\t\t\t\xDB\xDB\xDB\xDB\xB2 PLEASE ENTER YOUR CHOICE B/W 1 TO 9: ";
+    
+    int choice;
+    cin>>choice; 
+    
+    switch(choice)
+    {
+    	case 1:
+    		// Function to add book
+    		cout<<"hello";
+    	case 2:
+    		// Function to search book
+    		cout<<"hello";
+		
+		case 3:
+			// Function to add member
+			cout<<"hello";
+		
+		case 4:
+			// Function to see all book
+			cout<<"hello";
+		
+		case 5:
+			// Function to issue book
+			cout<<"hello";
+		
+		case 6:
+			// Function to return book
+			cout<<"hello";
+		
+		case 7:
+			// Function to show issued book
+			cout<<"hello";
+		
+		case 8:
+			// Function to add book
+			cout<<"hello";
+		
+		case 9:
+			// Function to add book
+			cout<<"hello";
+		
+	}
+}
